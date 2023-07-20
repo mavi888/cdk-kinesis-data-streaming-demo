f66832444a13e5e659c062720ab329c6e606701c
Villalba <lmv@f84d899822fc.ant.amazon.com>
2023-07-20 16:21:52 +0300

    Initial commit

---
+++ lib/cdk-kinesis-data-streaming-demo-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkKinesisDataStreamingDemoStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'CdkKinesisDataStreamingDemoQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
