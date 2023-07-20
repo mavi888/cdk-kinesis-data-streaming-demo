f18e737096177abed82ed8179c646b77a0eee688
Villalba <lmv@f84d899822fc.ant.amazon.com>
2023-07-20 18:10:12 +0300

    adding first commit

--- lib/cdk-kinesis-data-streaming-demo-stack.ts
+++ lib/cdk-kinesis-data-streaming-demo-stack.ts
@@ -1,16 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import { Stream, StreamMode } from 'aws-cdk-lib/aws-kinesis';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkKinesisDataStreamingDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkKinesisDataStreamingDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		const stream = new Stream(this, 'KDS', {
+			streamName: 'my-kinesis-data-stream',
+			streamMode: StreamMode.ON_DEMAND,
+		});
+	}
 }
