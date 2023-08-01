05b2c118695b00c9eba72e5a540fe48b792a0645
Villalba <lmv@f84d899822fc.ant.amazon.com>
2023-08-01 11:16:49 +0300

    Adding eda support

--- lib/cdk-kinesis-data-streaming-demo-stack.ts
+++ lib/cdk-kinesis-data-streaming-demo-stack.ts
@@ -1,6 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
+import { RemovalPolicy } from 'aws-cdk-lib';
+import { EventBus, Match, Rule } from 'aws-cdk-lib/aws-events';
+import { CloudWatchLogGroup } from 'aws-cdk-lib/aws-events-targets';
 import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { Stream, StreamMode } from 'aws-cdk-lib/aws-kinesis';
+import { LogGroup } from 'aws-cdk-lib/aws-logs';
 import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
 import { Construct } from 'constructs';
 
@@ -21,6 +25,7 @@ export class CdkKinesisDataStreamingDemoStack extends cdk.Stack {
 		const analyticsPipeRole = new Role(this, 'AnalyticsPipeRole', {
 			assumedBy: new ServicePrincipal('pipes.amazonaws.com'),
 		});
+
 		sourceStream.grantRead(analyticsPipeRole);
 		analyticsStream.grantWrite(analyticsPipeRole);
 
@@ -46,6 +51,57 @@ export class CdkKinesisDataStreamingDemoStack extends cdk.Stack {
 				kinesisStreamParameters: {
 					partitionKey: '$.partitionKey',
 				},
+			},
+		});
+
+		const eventBus = new EventBus(this, 'eventBus', {
+			eventBusName: 'kinesis-event-bus',
+		});
+
+		// All events on the eventBus are written to Amazon CloudWatch Logs for testing
+		const catchAllLogRule = new Rule(this, 'catchAllLogRule', {
+			ruleName: 'catchAllLogRule',
+			eventBus: eventBus,
+			eventPattern: {
+				source: Match.prefix(''),
+			},
+			targets: [
+				new CloudWatchLogGroup(
+					new LogGroup(this, 'kinesisEventBusCatchAllLogGroup', {
+						logGroupName:
+							'/aws/events/kinesisEventBusCatchAllLogGroup/catchAllLogGroup',
+						removalPolicy: RemovalPolicy.DESTROY,
+					})
+				),
+			],
+		});
+
+		const edaPipeRole = new Role(this, 'EdaPipeRole', {
+			assumedBy: new ServicePrincipal('pipes.amazonaws.com'),
+		});
+
+		eventBus.grantPutEventsTo(edaPipeRole);
+		sourceStream.grantRead(edaPipeRole);
+
+		const edaPipe = new CfnPipe(this, 'edapPipe', {
+			roleArn: edaPipeRole.roleArn,
+			source: sourceStream.streamArn,
+			sourceParameters: {
+				filterCriteria: {
+					filters: [
+						{
+							pattern: '{"data": {"event_type": ["EDA"] }}',
+						},
+					],
+				},
+				kinesisStreamParameters: {
+					startingPosition: 'LATEST',
+				},
+			},
+			target: eventBus.eventBusArn,
+			targetParameters: {
+				inputTemplate:
+					'{"event_type": <$.data.event_type>, "data": <$.data.some_data>, "partitionKey": <$.partitionKey>}',
 			},
 		});
 	}
